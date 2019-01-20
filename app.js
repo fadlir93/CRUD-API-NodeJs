dca415a46df1f61d96c76495dd147c02b26481aa
Fadli Ramadhan <fadlir93@gmail.com>
2019-01-20 08:08:23 +0700

    Define Route

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const product = require('./routes/product.route'); // import routes for the product
 const app = express();
+
+app.use('/products', product)
 
 let port = 1234;
 
