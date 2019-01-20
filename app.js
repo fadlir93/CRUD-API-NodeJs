9375bc93e5052d45241973f59472b20842ece6fd
Fadli Ramadhan <fadlir93@gmail.com>
2019-01-20 09:31:43 +0700

    Connectiong Database

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://userapi:abc123@ds123619.mlab.com:23619/productstutorial';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 const product = require('./routes/product.route'); // import routes for the product
 const app = express();
