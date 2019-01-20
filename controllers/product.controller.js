2b986c5c4b4f253561e60164c55ecc2ed5ffdf6f
Fadli Ramadhan <fadlir93@gmail.com>
2019-01-20 10:57:23 +0700

    Create Full API CRUD

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -19,3 +19,25 @@ exports.product_create = function (req, res) {
 exports.test = function(req, res) {
     res.send('Greetings from the test controller!');
 };
+
+exports.product_details = function(req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product)
+    })
+}
+
+exports.product_update = function(req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body},
+         function (err, product) {
+             if (err) return next (err);
+             res.send('Product update');
+         });
+}
+
+exports.product_delete = function(req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted Successfully !');
+    })
+};
